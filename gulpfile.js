5111fe569ce2a0323bcd486a9a84595ff4556264
Arfian Adam <Arfian Adam>
2016-08-16 16:33:55 +0700

    added gulp and webpack config

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    browserSync = require('browser-sync').create(),
+    webpack = require("webpack-stream"),
+    reload = browserSync.reload;
+
+gulp.task("serve", ["build"], function() {
+    browserSync.init({
+        server: {
+            baseDir: "./public/"
+        }
+    });
+
+    gulp.watch("./src/assets/**/*.scss", ["sass"]);
+    gulp.watch("./src/assets/**/*.js", ["js"]);
+    gulp.watch("./src/index.html", ["indexCopy"]);
+    gulp.watch("./public/**/*.*").on('change', reload);
+});
+
+gulp.task("sass", function() {
+    return gulp.src("./src/assets/css/style.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("./public/assets/css/"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task("js", function() {
+    return gulp.src("./src/assets/js/index.js")
+        .pipe(webpack(require("./webpack.config.js")))
+        .pipe(gulp.dest("./public/assets/js/"))
+});
+
+gulp.task("indexCopy", function() {
+    return gulp.src("./src/index.html")
+        .pipe(gulp.dest("./public/"))
+});
+
+gulp.task("build", ["sass", "js", "indexCopy"]);
+
+gulp.task("default", ["serve"]);
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: {
+        static: "./src/assets/js/index.js"
+    },
+    output: {
+        path: __dirname + "/public/assets/js/",
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: 'node_modules',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
+}
